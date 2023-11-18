10c355e2f0905b4bfa7832d2cb53cc59bc1f9271
sirius506775 <sirius506775@gmail.com>
2023-11-19 03:04:36 +0900

    feat : Create findCreditListByMid with Query

--- src/main/java/potato/hack/domain/credit/repository/CreditRepository.java
+++ src/main/java/potato/hack/domain/credit/repository/CreditRepository.java
@@ -1,7 +1,15 @@
-package potato.hack.domain.credit.repository;
+    package potato.hack.domain.credit.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import potato.hack.domain.credit.entity.Credit;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.jpa.repository.Query;
+    import org.springframework.data.repository.query.Param;
+    import potato.hack.domain.credit.entity.Credit;
 
-public interface CreditRepository extends JpaRepository<Credit, Long> {
-}
+    import java.util.List;
+
+    public interface CreditRepository extends JpaRepository<Credit, Long> {
+
+        @Query("SELECT c FROM Credit c WHERE c.member.mid = :mid")
+        List<Credit> findCreditListByMid(@Param("mid") String mid);
+
+    }
