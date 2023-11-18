3c77992c26d85655d643f37cc96fbcffb766542c
mythpoy <okmlnsunok@gmail.com>
2023-11-19 03:16:27 +0900

    test: Member 생성 테스트 변경

--- src/test/java/potato/hack/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/potato/hack/domain/member/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package potato.hack.domain.member.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 import potato.hack.domain.member.entity.Member;
 
@@ -12,13 +13,18 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
     @Rollback(value = false)
     void create_member() {
         Member jyp = Member.builder()
-                .mid("jyp")
-                .name("...@naver.com")
-                .password("1111")
+                .mid("dhl")
+                .name("이동헌")
+                .phone("01012341234")
+                .credit_total(2350)
+                .password(passwordEncoder.encode("1111"))
                 .build();
 
         memberRepository.save(jyp);
