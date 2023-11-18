6e0016e10aaf17f36122943844633d00252264e5
sirius506775 <sirius506775@gmail.com>
2023-11-19 03:04:17 +0900

    feat : Create Credit Service Layer

--- src/main/java/potato/hack/domain/credit/service/CreditServiceImpl.java
+++ src/main/java/potato/hack/domain/credit/service/CreditServiceImpl.java
@@ -3,19 +3,37 @@ package potato.hack.domain.credit.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import potato.hack.domain.credit.dto.CreditResponseDTO;
 import potato.hack.domain.credit.entity.Credit;
-import potato.hack.global.pageDTO.PageResponseDTO;
+import potato.hack.domain.credit.repository.CreditRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 @Log4j2
 public class CreditServiceImpl implements CreditService {
-    @Override
-    public PageResponseDTO<Credit> getCreditList() {
 
-        return null;
+    private final CreditRepository creditRepository;
+
+    @Override
+    public List<CreditResponseDTO> getCreditList(String mid) {
+        List<Credit> creditList = creditRepository.findCreditListByMid(mid);
+
+        return creditList.stream()
+                .map(this::entityDTO)
+                .collect(Collectors.toList());
+    }
+
+    private CreditResponseDTO entityDTO(Credit credit){
+        return CreditResponseDTO.builder()
+                .cno(credit.getCno())
+                .credit_value(credit.getCredit_value())
+                .cause_description(credit.getDescription())
+                .credit_status(credit.getCredit_status().toString())
+                .build();
     }
 }
